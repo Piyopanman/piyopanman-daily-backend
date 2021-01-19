55abc83e94ae0ee61eaccba79d1520727c37f5d9
Piyopanman <g19911kh@gm.tsuda.ac.jp>
2021-01-20 01:22:20 +0900

    titleとheaderを「ぴよぱんまんのサイト」にした:hatched_chick:

--- daily_api/urls.py
+++ daily_api/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_title = "ぴよぱんまんのサイト"
+admin.site.site_header = "ぴよぱんまんのサイト"
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('daily/', include('daily.urls')),

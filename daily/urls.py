787ffe03cf4537154ced4cea8d584ed1f6ccdccd
Piyopanman <g19911kh@gm.tsuda.ac.jp>
2021-03-18 12:35:13 +0900

    コメントの削除

--- daily/urls.py
+++ daily/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from . import views
 
-# localhost:8000/api/のあとの話
 urlpatterns = [
     path('', views.ListDaily.as_view()),
     path('post/<int:pk>/', views.DetailDaily.as_view()),
